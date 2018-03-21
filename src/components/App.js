03512f1446a4965d5f4fe7abf678ec1bfb8c7fa7
Anandraj <al.anandraj@gmail.com>
2018-03-21 06:30:12 +0530

    Twitter: initial commit with twitter button.

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+	render() {
+		return(
+			<div>
+				<div class="container">
+					<h2>Twitter</h2>
+						<button type="button" class="btn btn-primary">Login</button>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default App;
