340c9600541f85c8b44b38ab2467951c76495369
Amit Kumar <amitkumar2080799@gmail.com>
2018-03-23 11:15:00 +0530

    twitter oauth react js.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,29 @@
 import React, {Component} from 'react';
 
+import request from 'superagent';
+
 class App extends Component {
+
+	sendRequest = () => {
+		console.log("clickeddddddddddddddddddd");
+
+		request
+			.get('http://localhost:3002/request-token')
+			.end((error, result) => {
+				console.log("<<<<<<<<<>>>>>>>>> 111111111111", error);
+				console.log("<<<<<<<<<>>>>>>>>> 222222222222", result);
+			});
+		
+		// fetch("http://localhost:3002/request-token")
+		// 	.then(result=>console.log("<<<<<<<<<>>>>>>>>>", result.json()))
+	}
+
 	render() {
 		return(
 			<div>
-				<div class="container">
+				<div>
 					<h2>Twitter</h2>
-						<button type="button" class="btn btn-primary">Login</button>
+						<button onClick={this.sendRequest}>Login</button>
 				</div>
 			</div>
 		)
